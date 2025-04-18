17371e5c3a530ed7ed924257b8992aa0dfd93360
aun1414 <abbasaun1414@gmail.com>
2025-04-18 17:21:14 -0400

    fixed app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
