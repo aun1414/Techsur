cede77d51429301656a2caf3d157b214e7764f9f
aun1414 <abbasaun1414@gmail.com>
2025-05-07 20:47:52 -0400

    Redeploy

--- server/src/main/java/com/techsur/server/controller/MatchController.java
+++ server/src/main/java/com/techsur/server/controller/MatchController.java
@@ -67,7 +67,7 @@ public class MatchController {
             String email = SecurityContextHolder.getContext().getAuthentication().getName();
 
             // Prepare the request to Flask microservice
-            String flaskUrl = "http://18.223.143.214:5000/match";
+            String flaskUrl = "http://18.188.82.174/match";
 
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.MULTIPART_FORM_DATA);
